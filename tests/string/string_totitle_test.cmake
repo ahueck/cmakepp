db1b821c099330a696c5d71758c43229009c25af
Alexander Hück <hueck.alexander@gmail.com>
2015-03-25 17:00:16 +0100

    start of version with tokenizer: todo handle correct inline dots or brackets with a stack

--- tests/string/string_totitle_test.cmake
+++ tests/string/string_totitle_test.cmake
@@ -1,24 +1,107 @@
-function(test)
-  #set(str "abcdefg hello")
-  #string_totitle(${str})
-  #ans(res)
-  #assert("${res}" STREQUAL "Abcdefg Hello")
+function(test) 
+  #set(word "testing#tok(e)nizer#((a))#'subsentence')")##word#"Well:#!hello.#there?#'whathave
 
-  #set(res "")
-  #set(str "a abcdefg hello")
-  #string_totitle(${str})
-  #ans(res)
-  #assert("${res}" STREQUAL "A Abcdefg Hello")
+  ##string_encode_list(${word})
+  ##ans(str_encoded)
+  ##message("${str_encoded}")
 
-  #set(res "")
-  #set(str "a abcdefg hello superTrump?")
-  #string_totitle(${str})
-  #ans(res)
-  #assert("${res}" STREQUAL "A Abcdefg Hello superTrump?")
+  ##set(subsentence#"[!:.?']+")
+  ##set(bracket#"[\\(\\)]+")
+  ##set(bracketc#"[\\)]+")
+  ##set(other#"[^#!:.?'\\(\\)]+")##[a-zA-Z0-9]+
+  ##set(ws#"[#]+")
+  ###string(REGEX#REPLACE#"(${subsentence})|(${other})|(${ws})"#""#error#"${str_encoded}")
+  ###if(error)#
+  ###message("failed#to#tokenize#string")
+  ###return()
+  ###endif()
+  ##string(REGEX#MATCHALL#"(${subsentence})|(${ws})|(${bracket})|(${other})"#words#"${str_encoded}")
+  ##foreach(token#${words})
+  ##message("token=[${token}]")
+  ##endforeach()
+  ##return()
 
-  #set(res "")
-  set(str "!a abcdefg hello superTrump?")
+
+  # Every word upper case
+  set(str "abcdefg hello")
   string_totitle(${str})
   ans(res)
-  assert("${res}" STREQUAL "!A Abcdefg Hello superTrump?")
+  assert("${res}" STREQUAL "Abcdefg Hello")
+
+  set(res "")
+  # Small letter 'a' stays small
+  set(str "abcdefg a hello")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "Abcdefg a Hello")
+
+  set(res "")
+  # Keep whitespaces
+  set(str "abcdefg a   hello")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "Abcdefg a   Hello")
+
+  set(res "")
+  # words with dot inside stay small
+  set(str "abcdefg a hel.lo")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "Abcdefg a hel.lo")
+
+  set(res "")
+  # words with big letter elsewhere stay small
+  set(str "abcdefg a heLLo")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "Abcdefg a heLLo")
+
+  set(res "")
+  # beginning of sentence has 'small' -> uppercase
+  set(str "a abcdefg hello a superTrump?")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "A Abcdefg Hello a superTrump?")
+
+  set(res "")
+  # small after a subsentence (1/2)
+  set(str "Subsentence: a Sub")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "Subsentence: A Sub")
+  
+  set(res "")
+  # small after a subsentence (2/2)
+  set(str "Subsentence! a Sub")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "Subsentence! A Sub")
+
+  set(res "")
+  # v stays lower case
+  set(str "this v that")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "This v That")
+
+  set(res "")
+  # v changed to lower case
+  set(str "this V that")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "This v That")
+
+  set(res "")
+  # v changed to lower case
+  set(str "this Vs that")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "This vs That")
+
+  set(res "")
+  # v changed to lower case
+  set(str "this is not a colo(u)r?")
+  string_totitle(${str})
+  ans(res)
+  assert("${res}" STREQUAL "This Is Not colo(u)r?")
 endfunction()
