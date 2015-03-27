46cfaace1170a34ba62359060674ef5af4d8365e
Alexander Hück <hueck.alexander@gmail.com>
2015-03-27 18:49:43 +0100

    Simplified title case algorithm. TODO: should special chars inside a word leave case unaffected (e.g., colo(u)r)?

--- cmake/string/string_totitle.cmake
+++ cmake/string/string_totitle.cmake
@@ -1,24 +1,14 @@
 # transforms the specified string to title case
-# According to Stuart Colville Python implementation "titlecase 0.2"
-# MIT License
 function(string_totitle str)
   set(small a an and as at but by en for if in of on or the to via vs v v. vs. A An And As At But By En For If In Of On Or The To Via Vs V V. Vs.)
-
-  set(subsentence "[!:.?']+")
-  # groups all brackets (for our algorithm sufficient)
-  set(bracket_open "[\\(]+")
-  set(bracket_close "[\\(]+")
-  set(other "[^ !:.?'\\(\\)]+")
+  set(other "[^ ]+")
   set(ws "[ ]+")
-  #set(ucase_elsewhere "[a-zA-Z]+[A-Z]+")
+  set(is_subsentence true)
 
   string_encode_list(${str})
   ans(str_encoded)
-  string(REGEX MATCHALL "(${other})|(${subsentence})|(${ws})|(${bracket_open})|(${bracket_close})" tokens "${str_encoded}")
-
-  set(is_subsentence false)
-  # TODO: stack for correct handling of words like "t(e)st", "t(e)St" and inline periods etc.
-  #set(combine_stack)
+  string(REGEX MATCHALL "(${ws})|(${other})" tokens "${str_encoded}")
+  
   while(true)
     list(LENGTH tokens length)
     if(NOT length)
@@ -27,138 +17,43 @@ function(string_totitle str)
     list(GET tokens 0 token)
     list(REMOVE_AT tokens 0)
 
-    if("${token}" MATCHES "${ws}")
-    elseif("${token}" MATCHES "${subsentence}")
-      set(is_subsentence true)
-    elseif("${token}" MATCHES "${bracket_open}")
+   #message("Looking at=${token}")
+    if("${token}" MATCHES "^([^a-zA-Z0-9]*)([a-zA-Z])([a-z]*)([:?!']*)$")
+     #message("Matched=${token}")
+      set(pre ${CMAKE_MATCH_1})
+      set(first_letter ${CMAKE_MATCH_2})
+      set(lc_letters ${CMAKE_MATCH_3})
+      set(post ${CMAKE_MATCH_4})
       
-    elseif("${token}" MATCHES "${bracket_close}")
-      
-    elseif("${token}" MATCHES "${other}")
-      string(SUBSTRING "${token}" 0 1 first)
-      string(SUBSTRING "${token}" 1 -1 rest)
-
-      list(FIND small ${token} index)
+      list(FIND small "${first_letter}${lc_letters}" index)
       if(index GREATER -1)
+       #message("Found small=${token}")
         if(is_subsentence)
-          string(TOUPPER ${first} first)
+         #message("To upper small=${token}")
+          string(TOUPPER ${first_letter} first_letter)
         else()
-          string(TOLOWER ${first} first)
+         #message("To lower small=${token}")
+          string(TOLOWER ${first_letter} first_letter)
         endif()
-        set(token "${first}${rest}")
-      else()
-        # TODO 'uppercase elsewhere', 'inline period' -> keep existing case
-        string(TOUPPER ${first} first)
-        set(token "${first}${rest}")
+        set(token "${pre}${first_letter}${uc_letters}${lc_letters}${post}")
+      elseif(NOT uc_letters)
+       #message("Make upper case=${token}")
+        string(TOUPPER ${first_letter} first_letter)
+        set(token "${pre}${first_letter}${uc_letters}${lc_letters}${post}")
       endif()
-      set(is_subsentence false)
+      
+      #message("post=${post}")
+      if(NOT post)
+       #message("Subsentence disabled")
+        set(is_subsentence false)
+      else()
+       #message("Subsentence enabled")
+        set(is_subsentence true)
+      endif()
     endif()
 
     set(res "${res}${token}")
   endwhile()
+
   return_ref(res)
-
-# FIXME remove
-# set(is_sub false)
-# set(start true)
-# foreach(token ${tokens})
-#   message("Start token=[${token}]")
-#   if("${token}" MATCHES "${subsentence}")
-#     set(is_sub true)
-#   elseif("${token}" MATCHES "${other}")
-#     #message("Other token found = ${token}")
-#     # Until smarter way?
-#     string(SUBSTRING "${token}" 0 1 first)
-#     string(SUBSTRING "${token}" 1 -1 rest)
-#     
-#     list(FIND small ${token} index)
-#     #message("Token=${token} - ${index} (${first} ${rest})")
-#     if(index GREATER -1)
-#       # small token
-#       if(start OR is_sub)
-#         string(TOUPPER ${first} first)
-#         set(is_sub false)
-#       else()
-#         string(TOLOWER ${first} first)
-#       endif()
-#       set(token "${first}${rest}")
-#     elseif(NOT "${token}" MATCHES "${ucase_elsewhere}" AND NOT "${token}" MATCHES "${inline_period}")
-#       # ucase_elsewhere and inline_period -> keep existing case
-#       string(TOUPPER ${first} first)
-#       #message("token to upper")
-#       set(token "${first}${rest}")
-#     endif()
-#   endif()
-#   set(start false)
-
-#   set(res "${res}${token}")
-# endforeach()
-
-# return_ref(res)
-
-
-
-
-# string(REGEX MATCHALL "" words ${str_encoded})
-
-# set(ws " ")
-# string_split(${str} ${ws})
-# ans(words)
-
-# set(first true)
-# set(has_subsentence false)
-# foreach(word ${words})
-#   # extract small letter at beginning (TODO [:]* extend for subsentence)
-#   if(${word} MATCHES "([a-z])(.*)([:]*)")
-#     if(NOT ${CMAKE_MATCH_3} STREQUAL "")
-#       set(has_subsentence true)
-#     endif()
-#     # starts with lower case actually
-#     if(NOT ${CMAKE_MATCH_1} STREQUAL "")
-
-#     endif()
-
-
-#     list(FIND small ${word} index)
-#     if(idx GREATER 0)
-#       if(has_subsentence)
-#         set(has_subsentence false)
-#         string(TOUPPER "${word}" word)
-#         message("Line 24: small toupper")
-#       else()
-#         string(TOLOWER "${word}" word)
-#         message("Line 27: small tolower")
-#       endif()
-#     elseif(${word} MATCHES "([']*)([a-z])([a-z]*)([\\.\\?!:;]*)") 
-#       message("Matched word=${word}")
-#       string(TOUPPER ${CMAKE_MATCH_2} CMAKE_MATCH_2)
-#       set(word "${CMAKE_MATCH_1}${CMAKE_MATCH_2}${CMAKE_MATCH_3}${CMAKE_MATCH_4}")
-#       if(NOT ${CMAKE_MATCH_4} STREQUAL "")
-#         message("Line 27: Found subsentence in word")
-#         set(has_subsentence true)
-#       endif()
-#     endif()
-#   endif()
-
-#   # string_combine(" " res u_res)
-#   if(first)
-#     set(first false)
-#   else()
-#     set(res "${res}${ws}")
-#   endif()
-#   set(res "${res}${word}")
-# endforeach()
-
-  # Special case: beginning of word
-  #string(REGEX REPLACE "(^${PUNCT}*)(${SMALL})(.*)" "\\1;\\2;\\3" RESULT "${res}")
-  #list(GET RESULT 0 group0)
-  #list(GET RESULT 1 group1)
-  #list(GET RESULT 2 group2)
-  #string(SUBSTRING "${group1}" 0 1 char)
-  #string(TOUPPER "${char}" char)
-  #string_set_char_at(0 ${group1} ${char})
-  #ans(group1)
-  #set(res "${group0}${group1}${group2}")
-
-#  return_ref(res)
 endfunction()
--- tests/string/string_totitle_test.cmake
+++ tests/string/string_totitle_test.cmake
@@ -21,87 +21,105 @@ function(test)
   ##endforeach()
   ##return()
 
+    set(res "")
+  # v changed to lower case
+  set(str "this V that")
+  string_totitle("${str}")
+  ans(res)
+  assert("${res}" STREQUAL "This v That")
+#return()
 
+    set(res "")
+  # small after a subsentence (1/2)
+  set(str "Subsentence: a Sub")
+  string_totitle("${str}")
+  ans(res)
+
+
+
+  set(res "")
   # Every word upper case
   set(str "abcdefg hello")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "Abcdefg Hello")
 
   set(res "")
   # Small letter 'a' stays small
   set(str "abcdefg a hello")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "Abcdefg a Hello")
 
   set(res "")
   # Keep whitespaces
   set(str "abcdefg a   hello")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "Abcdefg a   Hello")
 
   set(res "")
   # words with dot inside stay small
   set(str "abcdefg a hel.lo")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "Abcdefg a hel.lo")
 
   set(res "")
   # words with big letter elsewhere stay small
   set(str "abcdefg a heLLo")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "Abcdefg a heLLo")
 
   set(res "")
   # beginning of sentence has 'small' -> uppercase
   set(str "a abcdefg hello a superTrump?")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "A Abcdefg Hello a superTrump?")
 
   set(res "")
   # small after a subsentence (1/2)
   set(str "Subsentence: a Sub")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "Subsentence: A Sub")
   
   set(res "")
   # small after a subsentence (2/2)
   set(str "Subsentence! a Sub")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "Subsentence! A Sub")
 
   set(res "")
   # v stays lower case
   set(str "this v that")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "This v That")
 
-  set(res "")
-  # v changed to lower case
-  set(str "this V that")
-  string_totitle(${str})
-  ans(res)
-  assert("${res}" STREQUAL "This v That")
 
   set(res "")
   # v changed to lower case
   set(str "this Vs that")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
   assert("${res}" STREQUAL "This vs That")
 
   set(res "")
   # v changed to lower case
   set(str "this is not a colo(u)r?")
-  string_totitle(${str})
+  string_totitle("${str}")
   ans(res)
-  assert("${res}" STREQUAL "This Is Not colo(u)r?")
+  assert("${res}" STREQUAL "This Is Not a colo(u)r?")
+
+  set(res "")
+  # v changed to lower case
+  set(str "'a test'")
+  string_totitle("${str}")
+  ans(res)
+  set(expected "'A Test'")
+  assert("${res}" STREQUAL "'A Test'")
 endfunction()
